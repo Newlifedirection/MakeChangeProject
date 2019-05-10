d5d1924066150b1ee46a5f19249b12e27d21166b
Newlifedirection <craig.dreiling@gmail.com>
2019-05-10 17:33:03 -0600

    Added a main method with comments.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,4 +1,10 @@
 
 public class MakeChangeApp {
+	public static void main(String[] args) {
+		
+		//Prompt user for value, and get input
+		
+		//Prompt user for amount tendered, and get imput
+	}
 
 }
