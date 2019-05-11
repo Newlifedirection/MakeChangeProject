93c3f5447ea3353d99248e027ae57d5757a9e085
Newlifedirection <craig.dreiling@gmail.com>
2019-05-11 09:23:20 -0600

    Scanner setup, ask user for pirce.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
 	public static void main(String[] args) {
+		Scanner kb = new kb.nextInt();
 		
+			System.out.print("Enter the intem price: ");
 		//Prompt user for value, and get input
 		
 		//Prompt user for amount tendered, and get imput
