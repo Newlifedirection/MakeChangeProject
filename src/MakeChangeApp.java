5371095926597e0d59325ec8f320a7d8d622e054
Newlifedirection <craig.dreiling@gmail.com>
2019-05-11 09:38:12 -0600

    Price entered and print out success

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class MakeChangeApp {
 	public static void main(String[] args) {
-		Scanner kb = new kb.nextInt();
+		Scanner kb = new Scanner(System.in);
 		
-			System.out.print("Enter the intem price: ");
+			System.out.print("Enter the intem price $: ");
 		//Prompt user for value, and get input
-		
-		//Prompt user for amount tendered, and get imput
+			double price = kb.nextDouble();
+			System.out.println("The price is $: " + price);
+		//Prompt user for amount tendered, and get input
 	}
 
 }
